abb444ee799910dbf93f7d9f44e4884afc103ebf
Jim <jamecl@gmail.com>
2025-12-06 18:14:02 -0700

    Configure base path for microsite
    Set the base path for the application.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // THIS LINE IS NEW: It tells the app it lives in the /microsite/ folder
+      base: "/microsite/",
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
